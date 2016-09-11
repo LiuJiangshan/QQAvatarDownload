8d938a91c4107ba449edf6e5c3d9291d4b32e5d4
LiuJiangshan <1015905004@qq.com>
2016-09-11 17:11:10 +0800

    ljs

--- src/DownThread.java
+++ src/DownThread.java
@@ -1,30 +1,14 @@
+import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Random;
 
+import javax.imageio.ImageIO;
+
 public class DownThread extends Thread
 {
 	Random random = new Random();
-
-	boolean checkMd5(File file)
-	{
-		long fileLength = file.length();
-		if (fileLength == 1034 || fileLength == 2986 || fileLength == 2055)
-		{
-			String md5 = Md5.getMD5(file);
-			if (md5.equals("c1bb6a4abacecb1ff0bbf838ab3a4e08") || md5.equals("9e981dc7788599d9372c8381d776c554")
-					|| md5.equals("3b42d62450ffcb05428b958512bea22"))
-			{
-				return false;
-			}
-		}
-		return true;
-	}
 
 	@Override
 	public void run()
@@ -34,9 +18,8 @@ public class DownThread extends Thread
 		while (true)
 		{
 			qq = "";
-			int numCount = random.nextInt(5) + 6;
-			numCount = 10;
-			for (int i = 0; i < numCount; i++)
+			
+			for (int i = 0; i < Main.qqNum; i++)
 			{
 				if (i == 0)
 					qq = (random.nextInt(9) + 1) + "";
@@ -49,20 +32,22 @@ public class DownThread extends Thread
 				continue;
 
 			String urlStr = "http://qlogo1.store.qq.com/qzone/" + qq + "/" + qq + "/640";
+
+			urlStr = "http://q4.qlogo.cn/g?b=qq&nk=" + qq + "&s=640";
+
 			HttpURLConnection con = null;
-			OutputStream out = null;
-			InputStream in = null;
+			BufferedImage bufferedImage = null;
 			try
 			{
 				con = (HttpURLConnection) new URL(urlStr).openConnection();
-				in = con.getInputStream();
-				out = new FileOutputStream(saveFile);
-				int i = 0;
-				while ((i = in.read()) != -1)
+				bufferedImage = ImageIO.read(con.getInputStream());
+				int w = bufferedImage.getWidth();
+				int h = bufferedImage.getHeight();
+				if (w > 40 && h > 40)
 				{
-					out.write(i);
+					System.out.println(qq + ":w=" + w + ",h=" + h);
+					ImageIO.write(bufferedImage, "jpg", saveFile);
 				}
-				out.flush();
 			}
 			catch (Exception e)
 			{
@@ -70,40 +55,12 @@ public class DownThread extends Thread
 			}
 			finally
 			{
-				if (in != null)
-				{
-					try
-					{
-						in.close();
-						in = null;
-					}
-					catch (IOException e)
-					{
-					}
-				}
-				if (out != null)
-				{
-					try
-					{
-						out.close();
-						out = null;
-					}
-					catch (IOException e)
-					{
-					}
-				}
+				bufferedImage = null;
 				if (con != null)
 				{
 					con.disconnect();
 					con = null;
 				}
-			}
-			if (saveFile != null && saveFile.exists())
-			{
-				if (checkMd5(saveFile))
-					System.out.println(qq);
-				else
-					saveFile.delete();
 			}
 		}
 	}
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,74 @@
 import java.io.File;
+import java.util.HashMap;
 
 public class Main
 {
 	static File dir = new File("head");
+	static HashMap<String, String> nullHead = new HashMap<>();
+	// qq位数
+	static int qqNum;
+	// 线程数量
+	static int threadNum;
 
 	public static void main(String[] args)
 	{
-		if (!dir.exists())
-			dir.mkdir();
-		for (int i = 0; i < 10; i++)
-			new DownThread().start();
-		// File[] files = new File(dir, "null").listFiles();
-		// for (File f : files)
-		// System.out.println(Md5.getMD5(f));
+		if (args.length != 2)
+		{
+			printDoc();
+			return;
+		}
+		else if (!isNumber(args[0]))
+		{
+			System.out.println("qq位数不是数字");
+			return;
+		}
+		else if (!isNumber(args[1]))
+		{
+			System.out.println("线程位数不是数字");
+			return;
+		}
+		else
+		{
+			qqNum = Integer.parseInt(args[0]);
+			threadNum = Integer.parseInt(args[1]);
+			System.out.println("qq位数:" + qqNum);
+			System.out.println("线程数量:" + threadNum);
+			System.out.println("5秒后开始");
+			for (int i = 5; i >= 0; i--)
+				try
+				{
+					System.out.println(i);
+					Thread.sleep(1000);
+				}
+				catch (InterruptedException e)
+				{
+				}
+			if (!dir.exists())
+				dir.mkdir();
+			for (int i = 0; i < threadNum; i++)
+				new DownThread().start();
+			// File[] files = new File(dir, "null").listFiles();
+			// for (File f : files)
+			// System.out.println(Md5.getMD5(f));
+		}
+	}
+
+	static boolean isNumber(String numStr)
+	{
+		try
+		{
+			Integer.parseInt(numStr);
+		}
+		catch (Exception e)
+		{
+			return false;
+		}
+		return true;
+	}
+
+	static void printDoc()
+	{
+		System.out.println("使用:");
+		System.out.println("\tjava Main qq位数 线程数量");
 	}
 }
